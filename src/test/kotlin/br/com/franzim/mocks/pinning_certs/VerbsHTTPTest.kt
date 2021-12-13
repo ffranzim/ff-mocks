5fb11621c7c10c7bd4af6d6efb5f00983ccd2c90
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-12-13 17:30:27 -0300

    ajuste procfile

--- src/test/kotlin/br/com/franzim/mocks/pinning_certs/VerbsHTTPTest.kt
+++ src/test/kotlin/br/com/franzim/mocks/pinning_certs/VerbsHTTPTest.kt
@@ -3,6 +3,7 @@ package br.com.franzim.mocks.pinning_certs
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -11,10 +12,11 @@ class VerbsHTTPTest {
     @Test
     fun testHelloEndpoint() {
         given()
-          .`when`().get("/hello")
+          .`when`().get("/mock-no-ar")
           .then()
              .statusCode(200)
-             .body(`is`("Hello RESTEasy"))
+             .body(containsString("Mock está no ar!"))
+//             .body(`is`("Hello RESTEasy"))
     }
 
 }
